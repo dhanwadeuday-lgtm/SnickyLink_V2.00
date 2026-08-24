7fcbe378e73ac6a5905a2c9c9fd2436295158048
dhanwadeuday-lgtm <dhanwade.uday@gmail.com>
2026-08-24 20:55:15 +0530

    Delete webapp directory

--- webapp/migrations/0004_community_photos.sql
+++
@@ -1,4 +0,0 @@
--- Add photo support to community posts
-ALTER TABLE posts ADD COLUMN image_key TEXT;
-ALTER TABLE posts ADD COLUMN image_width INTEGER;
-ALTER TABLE posts ADD COLUMN image_height INTEGER;
